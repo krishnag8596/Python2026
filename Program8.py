be46af8a868f3f9aedcaef5e3e98e8e3228913d5
Krishna Gupta <149130535+krishnag8596@users.noreply.github.com>
2026-09-15 11:18:27 +0530

    Add files via upload
    Program Demonstrating Calculation of Average Marks and Result of Student on basis of user defined 5 subject Marks

---
+++ Program8.py
@@ -0,0 +1,10 @@
+mark1=int(input("Enter your marks 1: "));
+mark2=int(input("Enter your marks 2: "));
+marks3=int(input("Enter your marks 3: "));
+marks4=int(input("Enter your marks 4: "));
+marks5=int(input("Enter your marks 5: "));
+total=mark1+mark2+marks3+marks4+marks5;
+avg=total/3;
+
+print("The average Marks is ",avg);
+print("total marks",total)
