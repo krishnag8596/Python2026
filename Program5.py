e6c3ddd4aecc29b23a8b465eb82d848c1500b205
Krishna Gupta <149130535+krishnag8596@users.noreply.github.com>
2026-09-15 11:21:11 +0530

    Add files via upload
    Program Demonstrating Taking user input and displaying it

---
+++ Program5.py
@@ -0,0 +1,7 @@
+name=input("Enter Student Name :");
+roll_no =int(input("Enter Roll Number :"));
+marks=float(input("Enter Marks :"));
+
+print("Student Name :",name);
+print("Roll Number :",roll_no);
+print("Marks :",marks);
