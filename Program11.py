106c86264476e32116e60ac5e37b32d22f884698
Krishna Gupta <149130535+krishnag8596@users.noreply.github.com>
2026-09-15 11:20:13 +0530

    Add files via upload
    Program Demonstrating All Basic String Manipulation Techniques

---
+++ Program11.py
@@ -0,0 +1,28 @@
+#String Indexing
+
+s1="Hello World";
+'''print(s1);
+
+print(s1.upper());
+print(s1.lower());
+#print(s1.capitalize());
+print(s1[1]);
+print(s1[1:3]);
+print(s1[1:7]);
+print(s1[1:]);
+
+print(s1[:]);
+
+print(s1[-1]);
+print(s1[-2]);'''
+
+print(s1[::2]);
+print(s1[::3]);
+print(s1[::-1        M
+
+
+
+
+
+
+]);
